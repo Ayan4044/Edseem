ece03da0ddcc2c6a579fa22f17dbfcfaf51c64c9
Ayan404 <ayan.bhatt20@gmail.com>
2021-09-22 06:21:32 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Edseem
+ Level
---
+++ vintage_games/lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  var containerColor = Colors.blue.shade400;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Vintage Games",
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Bruce’s Retro Games"),
+            backgroundColor: Colors.blue,
+          ),
+          body: _gridView(),
+        ));
+
+    throw UnimplementedError();
+  }
+
+  Widget _gridView() {
+    return GridView.count(
+      primary: false,
+      padding: const EdgeInsets.all(20),
+      crossAxisSpacing: 10,
+      mainAxisSpacing: 10,
+      crossAxisCount: 2,
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.all(50),
+          child:
+              const Text("Collection 1", style: TextStyle(color: Colors.white)),
+          color: containerColor,
+        ),
+        Container(
+          padding: const EdgeInsets.all(50),
+          child:
+              const Text('Collection 2', style: TextStyle(color: Colors.white)),
+          color: containerColor,
+        ),
+        Container(
+          padding: const EdgeInsets.all(50),
+          child: const Text('Collection 3', style: TextStyle(color: Colors.white)),
+          color: containerColor,
+        ),
+        Container(
+          padding: const EdgeInsets.all(50),
+          child: const Text('Collection 4', style: TextStyle(color: Colors.white)),
+          color: containerColor,
+        ),
+        Container(
+          padding: const EdgeInsets.all(50),
+          child: const Text('Collection 5', style: TextStyle(color: Colors.white)),
+          color: containerColor,
+        ),
+        Container(
+          padding: const EdgeInsets.all(50),
+          child: const Text('Collection 6', style: TextStyle(color: Colors.white)),
+          color: containerColor,
+        ),
+      ],
+    );
+  }
+}
